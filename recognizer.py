9f8946e60a3f59f83ffafc3c8eaf494ff4880d31
Cieniu <marcin@laret.pl>
2017-03-11 23:05:02 +0100

    add model

--- recognizer.py
+++ recognizer.py
@@ -9,7 +9,7 @@ class Recognizer():
         p1 = subprocess.call('rec record.wav rate 16k silence 1 0.1 3% 1 3.0 3%',shell=True)
         time.sleep(1)
         print 'recognizing...'
-        recognized_text = subprocess.check_output('pocketsphinx_continuous -infile record.wav -logfn /dev/null',shell=True, stderr=subprocess.STDOUT)
+        recognized_text = subprocess.check_output('pocketsphinx_continuous -infile record.wav -lm models/5835.lm -dict models/5835.dic -logfn /dev/null',shell=True, stderr=subprocess.STDOUT)
         os.remove('record.wav')
         return recognized_text
 
